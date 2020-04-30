b63d31b79279df88932f4fad20791606992070e4
Milagros Pielvitori <mpielvitori@sistran.com.ar>
2020-04-30 17:04:13 -0300

    error en PPT

--- src/app/clases/juego-piedra-papel-tijera.ts
+++ src/app/clases/juego-piedra-papel-tijera.ts
@@ -1,2 +1,80 @@
-export class JuegoPiedraPapelTijera {
+import { Juego } from '../clases/juego'
+
+export class JuegoPiedraPapelTijera extends Juego {
+
+    //Clase Juego
+    constructor(nombre?: string, gano?: boolean, jugador?: string) {
+        super("Piedra Papel o Tijera", gano, jugador);
+
+    }
+
+    //Verificar empate
+    public verificar() {
+        if (this.eleccionPC == this.eleccionHumano) {
+            return true;
+        }
+        return false;
+    }
+    //
+
+    eleccionesPC: string[] = ['Piedra', 'Papel', 'Tijera'];
+    eleccionPC: string;
+    eleccionHumano: string;
+
+
+
+    public generarJuegoPC() {
+        var aleatorio: number;
+        aleatorio = Math.floor(Math.random() * (this.eleccionesPC.length));
+        this.eleccionPC = this.eleccionesPC[aleatorio]
+
+        console.info('Elección PC: ' + this.eleccionPC);
+        this.gano = false;
+    }
+
+    public verificarResultado() {
+        var empate;
+        var resultado;
+        empate = this.verificar();
+        if (!empate) {
+            switch (this.eleccionHumano) {
+                case ('Piedra'):
+                    switch (this.eleccionPC) {
+                        case ('Papel'):
+                            resultado = -1;
+                            break;
+                        case ('Tijera'):
+                            resultado = 1;
+                            break;
+                    }
+                    break;
+                case ('Papel'):
+                    switch (this.eleccionPC) {
+                        case ('Piedra'):
+                            resultado = 1;
+                            break;
+                        case ('Tijera'):
+                            resultado = -1;
+                            break;
+                    }
+                    break;
+                case ('Tijera'):
+                    switch (this.eleccionPC) {
+                        case ('Papel'):
+                            resultado = 1;
+                            break;
+                        case ('Piedra'):
+                            resultado = -1;
+                            break;
+                    }
+                    break;
+            }
+        }else{
+            resultado=0;
+        }
+        return resultado;
+    }
+
+
+
 }
--- src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JuegoPiedraPapelTijera } from '../../clases/juego-piedra-papel-tijera';
 
 @Component({
   selector: 'app-piedra-papel-tijera',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PiedraPapelTijeraComponent implements OnInit {
 
-  constructor() { }
+  nuevoJuego: JuegoPiedraPapelTijera;
+  winner:boolean=false;
+  loser:boolean=false;
+  tie:boolean=false;
+
+
+  constructor() { 
+    this.nuevoJuego = new JuegoPiedraPapelTijera();
+    console.info("Inicio Piedra papel o tijera");
+  }
 
   ngOnInit(): void {
+  }
+
+  NuevoJuego(){
+    this.nuevoJuego.generarJuegoPC();
+    this.winner=false;
+    this.loser = false;
+    this.tie = false;
+
+  }
+
+  verificar(){
+    var resultado;
+    resultado= this.nuevoJuego.verificarResultado();
+    if(resultado==1){
+      this.winner=true;
+    }else if(resultado==0){
+      this.tie=true;
+    }else{
+      this.loser=true;
+    }
+
+
   }
 
 }
