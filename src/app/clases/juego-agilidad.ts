9f55373168f7743b809d02ce67e4a09802141b4b
Milagros Pielvitori <mpielvitori@sistran.com.ar>
2020-04-30 14:15:26 -0300

    Estilos y logica juego 1

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,6 +1,61 @@
-export class JuegoAgilidad {
+import { Juego } from '../clases/juego'
+
+export class JuegoAgilidad extends Juego {
 
 
-    gano:boolean;
-    numeroIngresado; 
+    gano: boolean;
+    numeroIngresado: number;
+    operadores: string[] = ['+', '-', '*', '/'];
+    operador: string;
+    primerNumero: number;
+    segundoNumero: number;
+    resultado: number;
+
+    //Clase Juego
+    constructor(nombre?: string, gano?: boolean, jugador?: string) {
+        super("Adivina el número", gano, jugador);
+
+    }
+
+    public verificar() {
+        if (this.numeroIngresado == this.resultado) {
+            this.gano = true;
+        }
+        if (this.gano) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //
+
+    public generarnumeros() {
+        var aleatorio: number;
+        this.primerNumero = Math.floor((Math.random() * 100) + 1);
+        this.segundoNumero = Math.floor((Math.random() * 100) + 1);
+        aleatorio = Math.floor(Math.random() * (this.operadores.length));
+        this.operador = this.operadores[aleatorio]
+
+        switch (this.operador) {
+            case '*':
+                this.resultado = this.primerNumero * this.segundoNumero;
+                break;
+            case '/':
+                this.resultado = this.primerNumero / this.segundoNumero;
+                break;
+            case '+':
+                this.resultado = this.primerNumero + this.segundoNumero;
+                break;
+            case '-':
+                this.resultado = this.primerNumero - this.segundoNumero;
+                break;
+        }
+
+        console.info('Números: ' + this.primerNumero + ' ' + this.operador + ' ' + this.segundoNumero + ' ' + this.resultado);
+        this.gano = false;
+    }
+
+
+
+
 }
--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -1,51 +1,80 @@
-import { Component, OnInit ,Input,Output,EventEmitter} from '@angular/core';
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
 import { JuegoAgilidad } from '../../clases/juego-agilidad'
 
-import {Subscription} from "rxjs";
-import {TimerObservable} from "rxjs/observable/TimerObservable";
+import { Subscription } from "rxjs";
+import { TimerObservable } from "rxjs/observable/TimerObservable";
 @Component({
   selector: 'app-agilidad-aritmetica',
   templateUrl: './agilidad-aritmetica.component.html',
   styleUrls: ['./agilidad-aritmetica.component.css']
 })
 export class AgilidadAritmeticaComponent implements OnInit {
-   @Output() 
-  enviarJuego :EventEmitter<any>= new EventEmitter<any>();
-  nuevoJuego : JuegoAgilidad;
+  @Output() enviarJuego: EventEmitter<any> = new EventEmitter<any>();
+  nuevoJuego: JuegoAgilidad;
   ocultarVerificar: boolean;
   Tiempo: number;
-  repetidor:any;
+  repetidor: any;
+
+  winner:boolean=false;
+  loser:boolean=false;
   private subscription: Subscription;
+
   ngOnInit() {
   }
-   constructor() {
-     this.ocultarVerificar=true;
-     this.Tiempo=5; 
+  constructor() {
+    this.ocultarVerificar = true;
+    this.Tiempo = 5;
     this.nuevoJuego = new JuegoAgilidad();
-    console.info("Inicio agilidad");  
+    console.info("Inicio agilidad");
   }
   NuevoJuego() {
-    this.ocultarVerificar=false;
-   this.repetidor = setInterval(()=>{ 
-      
+    this.ocultarVerificar = false;
+    this.winner=false;
+    this.loser = false;
+    this.nuevoJuego.generarnumeros();
+    this.repetidor = setInterval(() => {
+
       this.Tiempo--;
       console.log("llego", this.Tiempo);
-      if(this.Tiempo==0 ) {
+      if (this.Tiempo == 0) {
         clearInterval(this.repetidor);
         this.verificar();
-        this.ocultarVerificar=true;
-        this.Tiempo=5;
+        this.ocultarVerificar = true;
+        this.Tiempo = 5;
       }
-      }, 900);
+    }, 900);
 
   }
-  verificar()
-  {
-    this.ocultarVerificar=false;
+  verificar() {
+    this.ocultarVerificar = false;
     clearInterval(this.repetidor);
-   
 
-   
-  }  
+    if(this.nuevoJuego.verificar()){
+      this.MostrarMensaje(true);
+    } else{
+      this.MostrarMensaje(false);
+    }
+
+
+
+  }
+
+  MostrarMensaje(ganador:boolean) {
+    if(ganador)
+      {
+        this.winner=true;
+      }else{
+        this.loser=true;
+      }
+    setTimeout(function(){ 
+      this.winner=false;
+      this.loser=false;
+     }, 3000);
+  
+   } 
+
+
+
+
 
 }
