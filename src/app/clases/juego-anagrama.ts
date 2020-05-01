9974e06aa85046bcd4f2318ff36e44b91181fb2a
Milagros Pielvitori <mpielvitori@sistran.com.ar>
2020-05-01 12:13:15 -0300

    anagrama

--- src/app/clases/juego-anagrama.ts
+++ src/app/clases/juego-anagrama.ts
@@ -1,2 +1,48 @@
-export class JuegoAnagrama {
+import { Juego } from './juego';
+
+export class JuegoAnagrama extends Juego{
+
+    gano: boolean;
+    respuesta: string;
+
+    anagramas:string[]=['T-R-I-B-U-N-O','P-O-R-C-I-O-N-E-S','G-L-O-R-I-A','P-R-E-C-I-O','R-U-B-O-R','N-O-T-A-R','A-M-I-G-O-C-R-U-E-L','B-A-R-C-O','P-A-S-I-V-A'];
+    anagrama:string;
+    correctas:string[]=['TIBURON','ESCORPION','GORILA','PERICO','BURRO','RATON','MURCIELAGO','COBRA','AVISPA'];
+    correcta:string;
+    
+
+
+
+    //Clase Juego
+    constructor(nombre?: string, gano?: boolean, jugador?: string) {
+        super("Adivina el animal", gano, jugador);
+
+    }
+
+    public verificar() {
+        if (this.respuesta.toUpperCase() == this.correcta) {
+            this.gano = true;
+        }
+        if (this.gano) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //
+
+    public generarpalabra() {
+        var aleatorio: number;
+        aleatorio = Math.floor(Math.random() * (this.anagramas.length));
+        this.anagrama = this.anagramas[aleatorio];
+        this.correcta = this.correctas[aleatorio];
+
+        console.info('Palabra: ' + this.correcta + '--- Solución: ' + this.correcta);
+        this.gano = false;
+    }
+
+
+
+
+
 }
--- src/app/componentes/anagrama/anagrama.component.ts
+++ src/app/componentes/anagrama/anagrama.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { JuegoAnagrama } from '../../clases/juego-anagrama';
+
 
 @Component({
   selector: 'app-anagrama',
@@ -7,9 +11,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnagramaComponent implements OnInit {
 
-  constructor() { }
+
+  @Output() enviarJuego: EventEmitter<any> = new EventEmitter<any>();
+  nuevoJuego: JuegoAnagrama;
+  ocultarVerificar: boolean;
+  Tiempo: number;
+  repetidor: any;
+
+  winner:boolean=false;
+  loser:boolean=false;
+  private subscription: Subscription;
+
 
   ngOnInit() {
   }
+
+  constructor() {
+    this.ocultarVerificar = true;
+    this.Tiempo = 10;
+    this.nuevoJuego = new JuegoAnagrama();
+    console.info("Inicio Anagrama");
+  }
+  NuevoJuego() {
+    this.ocultarVerificar = false;
+    this.winner=false;
+    this.loser = false;
+    this.nuevoJuego.generarpalabra();
+    this.repetidor = setInterval(() => {
+
+      this.Tiempo--;
+      console.log("quedan", this.Tiempo);
+      if (this.Tiempo == 0) {
+        clearInterval(this.repetidor);
+        this.verificar();
+        this.ocultarVerificar = true;
+        this.Tiempo = 10;
+      }
+    }, 900);
+
+  }
+  verificar() {
+    this.ocultarVerificar = false;
+    clearInterval(this.repetidor);
+
+    if(this.nuevoJuego.verificar()){
+      this.MostrarMensaje(true);
+    } else{
+      this.MostrarMensaje(false);
+    }
+
+
+
+  }
+
+  MostrarMensaje(ganador:boolean) {
+    if(ganador)
+      {
+        this.winner=true;
+      }else{
+        this.loser=true;
+      }
+    setTimeout(function(){ 
+      this.winner=false;
+      this.loser=false;
+     }, 3000);
+  
+   } 
+
+
+
 
 }
